434ab4801322c718d0134228317da0b553e68ac2
belledon <mbelledonne@gmail.com>
2020-09-29 14:51:36 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -14,10 +14,12 @@ sudo apt-get update && sudo apt-get install -y \
     cryptsetup
 
 
-export VERSION=1.13 OS=linux ARCH=amd64 && \  # Replace the values as needed
-  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \ # Downloads the required Go package
-  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \ # Extracts the archive
-  rm go$VERSION.$OS-$ARCH.tar.gz    # Deletes the ``tar`` file
+sudo apt-get clean
+
+export VERSION=1.13 OS=linux ARCH=amd64  # Replace the values as needed
+wget "https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz" # Downloads the required Go package
+sudo tar -C /usr/local -xzvf "go${VERSION}.${OS}-${ARCH}.tar.gz"  # Extracts the archive
+rm go${VERSION}.${OS}-${ARCH}.tar.gz    # Deletes the ``tar`` file
 
 export PATH=/usr/local/go/bin:$PATH
 
